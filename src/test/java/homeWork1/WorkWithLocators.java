ea2fef6ca9201a8de6c72eb82440a9f8bc7df74b
vbabiak <metallbv@gmail.com>
2019-08-02 16:24:28 +0200

    Add window maximize.

--- src/test/java/homeWork1/WorkWithLocators.java
+++ src/test/java/homeWork1/WorkWithLocators.java
@@ -28,7 +28,8 @@ public class WorkWithLocators {
   public void setUp() {
     System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\chromedriver.exe");
     driver = new ChromeDriver();
-    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
   }
 
   @Test
