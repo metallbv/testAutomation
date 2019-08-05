81835b410debb01b2806d8a3bf95200c34263e89
vbabiak <metallbv@gmail.com>
2019-08-05 07:50:42 +0300

    Home work 2. Added Login object.

---
+++ src/main/java/HomePage.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+
+  private final WebDriver driver;
+
+  public HomePage(WebDriver driver) {
+    this.driver = driver;
+  }
+
+}
---
+++ src/main/java/LoginPage.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+  By usernameLocator = By.id("signInEmail");
+  By passwordLocator = By.id("signInPassword");
+  By loginButtonLocator = By.xpath("//input[@class=\"popup-reg-sign-in-form__sign-in\"]");
+
+  private final WebDriver driver;
+
+  public LoginPage(WebDriver driver) {
+    this.driver = driver;
+
+    if (!"Login".equals(driver.getTitle())) {
+      throw new IllegalStateException("This is not the login page");
+    }
+  }
+
+  public LoginPage typeUsername(String username) {
+    driver.findElement(usernameLocator).sendKeys(username);
+    return this;
+  }
+
+  public LoginPage typePassword(String password) {
+    driver.findElement(passwordLocator).sendKeys(password);
+    return this;
+  }
+
+  public HomePage submitLogin() {
+    driver.findElement(loginButtonLocator).submit();
+    return new HomePage(driver);
+  }
+
+  public LoginPage submitLoginExpectingFailure() {
+    driver.findElement(loginButtonLocator).submit();
+    return new LoginPage(driver);
+  }
+
+  public HomePage loginAs(String username, String password) {
+    typeUsername(username);
+    typePassword(password);
+    return submitLogin();
+  }
+
+}
